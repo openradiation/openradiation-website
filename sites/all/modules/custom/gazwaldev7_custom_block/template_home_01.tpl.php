24370530f66fc3a0a27e998019acc9f6f33e2bff
ghisd <ghisd@example.com>
2020-03-18 18:24:51 +0100

    add in flight measurements

--- sites/all/modules/custom/gazwaldev7_custom_block/template_home_01.tpl.php
+++ sites/all/modules/custom/gazwaldev7_custom_block/template_home_01.tpl.php
@@ -5,8 +5,9 @@
      'width' => '100%'
   ))
 */
+global $language;
 $url = variable_get('openradiation_server_rest_request_api_url', '');
-print "<iframe id=\"openradiation-map-iframe\" frameborder=\"0\" style=\"overflow:hidden;height:100%;width:100%\" height=\"100%\" width=\"100%\" src=\"$url/openradiation\"></iframe>";
+print "<iframe id=\"openradiation-map-iframe\" frameborder=\"0\" style=\"overflow:hidden;height:100%;width:100%\" height=\"100%\" width=\"100%\" src=\"$url/$language->language/openradiation\"></iframe>";
 
 ?>
 
--- sites/all/themes/bootsass/templates/node/node--measurement.tpl.php
+++ sites/all/themes/bootsass/templates/node/node--measurement.tpl.php
@@ -67,13 +67,17 @@ hide($content['links']);
           </div>
           <div class="col-md-12 nopadding">
             <ul class="list-unstyled details">
+              <li></li>
               <li><span class="atypical" ><?php if (isset($content['field_atypical_measurement']['#items'][0]['value']) && ("1" == $content['field_atypical_measurement']['#items'][0]['value'])) print t('Atypical measurement') ?></span></li>
               <li><span class="blabel"><?php print t('Beginning date') ?> : </span><?php print format_date($content["field_time"]['#items'][0]['value'], $type = 'custom', $format = 'l, j F, Y - H:i:s') ?></li>
+              <li><span class="blabel"><?php print t('Latitude') ?> : </span><?php print check_plain($content['field_geolocalisation']['#items'][0]['lat']) ?></li>
+              <li><span class="blabel"><?php print t('Longitude') ?> : </span><?php print check_plain($content['field_geolocalisation']['#items'][0]['lng']) ?></li>
+              <li><span class="blabel"><?php print t('Altitude (m)') ?> : </span><?php print render($content['field_altitude']) ?></li>             
               <li><span class="blabel"><?php print t('Measurement duration : ') ?></span><?php
                   $date1 = $content["field_time"]['#items'][0]['value'];
                   $date2 = $content["field_time"]['#items'][0]['value2'];
                   if (empty($content["field_time"]['#items'][0]['value2']) || ($date1 == $date2) ) {
-                    print t('NA');
+                    print t(' - ');   /*print t('NA');*/
                   } else {
                     $newdate1 = new DateObject($date1);
                     $newdate2 = new DateObject($date2);
@@ -81,10 +85,38 @@ hide($content['links']);
                     print render($interv);
                   }
                   ?>
-                  </li>
+              </li>             
+
+              <li><span class="blabel"><?php print t('Distance travel during measure') ?> : </span>
+              <?php if (!empty($content["field_geolocation"]['#items'][0]['latEnd']) && !empty($content["field_geolocation"]['#items'][0]['latEnd'])) {
+ 		$lat = (float) check_plain($content['field_geolocalisation']['#items'][0]['lat']) * pi()/180;
+  		$lng = (float) check_plain($content['field_geolocalisation']['#items'][0]['lng']) * pi()/180;
+                $latEnd = (float) check_plain($content['field_geolocalisation']['#items'][0]['latEnd']) * pi()/180;
+                $lngEnd = (float) check_plain($content['field_geolocalisation']['#items'][0]['lngEnd']) * pi()/180;
+		$alt = 0;
+		if (isset($content['field_altitude']['#items'][0]['value'])) {
+			$alt = (float) check_plain($content['field_altitude']);
+		}
+      		$distance = round(acos(sin($lat)*sin($latEnd) + cos($lat)*cos($latEnd)*cos($lng-$lngEnd))  * (6366*1000 + $alt), 0);
+		print $distance;
+		print (' m');
+		if (!empty($content["field_time"]['#items'][0]['value2'])) {
+		   $duration = time_getDiffInSecond($content["field_time"]['#items'][0]['value2'],$content["field_time"]['#items'][0]['value']);
+                   $speed = round((($duration*3600)/($distance*1000)),0); 
+                   print(' (');
+                   print($speed);
+		   print(' km/h)');
+                } 
+	      } ?>	
+	      </li>
               <li><span class="blabel"><?php print t('Measurement environment') ?> : </span><?php print render($content["field_measurementenvironment"]) ?></li>
-              <li><span class="blabel"><?php print t('Latitude') ?> : </span><?php print check_plain($content['field_geolocalisation']['#items'][0]['lat']) ?></li>
-              <li><span class="blabel"><?php print t('Longitude') ?> : </span><?php print check_plain($content['field_geolocalisation']['#items'][0]['lng']) ?></li>
+              <?php
+              if ($content["field_measurementenvironment"][0]['#markup'] == 'Plane' || $content["field_measurementenvironment"][0]['#markup'] == 'Avion') {
+                showItemsPlane($content);
+              } else {
+                showRain($content);
+              } ?>
+
               <li><span class="blabel"><?php print t('Tags') ?> : </span><?php print render($content['field_measure_tags']) ?></li>
               <li><span class="blabel"><?php print t('Description') ?> : </span><span class="description"><?php print render($content['field_description']) ?></span></li>
               <li style="text-align:center; display:block;"><?php print render($content['field_measurement_photo']) ?></li>
@@ -94,7 +126,6 @@ hide($content['links']);
                 <li><span class="blabel"><?php print t('Measurement height above the ground (m)') ?> : </span><?php print render($content['field_measurement_height']) ?></li>
                 <li><span class="blabel"><?php print t('Temperature') ?> : </span><?php print render($content['field_temperature']) ?></li>
                 <li><span class="blabel"><?php print t('Hits number') ?> : </span><?php print render($content['field_hits_number']) ?></li>
-                <li><span class="blabel"><?php print t('Altitude (m)') ?> : </span><?php print render($content['field_altitude']) ?></li>
                 <li><span class="blabel"><?php print t('Measurement UUID') ?> : </span><?php print $node->uuid; ?></li>
                 <li><span class="blabel"><?php print t('Manual Reporting') ?> : </span><?php if ("1"==$content['field_manual_reporting']['#items'][0]['value']) print t('Yes'); else print t('No'); ?></li>
                 
@@ -140,3 +171,71 @@ hide($content['links']);
   </div>
 
 </div>
+
+
+<?php
+
+    // show if measurement environment is not plane
+  function showRain($content) {
+    print('<li><span class="blabel" >');
+    print t('Weather');
+    print(' : </span>');
+    if (isset($content['field_rain']['#items'][0]['value'])) {
+	if ("1" == $content['field_rain']['#items'][0]['value']) {
+         	print("<img src='/sites/all/themes/bootsass/assets/images/icon-rain-on.png' width='20' height='20' style='vertical-align: top;' /> ");
+		print t('Rain');
+        } else {
+            	print("<img src='/sites/all/themes/bootsass/assets/images/icon-sun-on.png' width='20' height='20' style='vertical-align: top;' /> ");
+         	print t('No rain');
+	}
+     } 
+    print ('</li>');
+  }
+
+  // show if measurement environment is plane
+  function showItemsPlane($content) {
+    print ('<li><span class="blabel">');
+    print t('Flight number');
+    print (' : </span>');
+    print render($content['field_flight_number']);
+    print ('</li>');
+
+    print ('<li><span class="blabel">');
+    print t('Seat number');
+    print (' : </span>');
+    print render($content['field_seat_number']);
+    print ('</li>');
+
+    print('<li><span class="blabel" >');
+    print t('Weather');
+    print(' : </span>');
+    if (isset($content['field_storm']['#items'][0]['value'])) {
+        if ("1" == $content['field_storm']['#items'][0]['value']) {
+                print("<img src='/sites/all/themes/bootsass/assets/images/icon-plane-storm-on.png' width='20' height='20' style='vertical-align: top;' /> ");
+                print t('Storm crossing');
+        } else {
+                print("<img src='/sites/all/themes/bootsass/assets/images/env_plane.png' width='20' height='20' style='vertical-align: top;' /> ");
+                print t('No storm');
+        }
+     }
+    print ('</li>');
+
+    print('<li><span class="blabel" >');
+    print t('Seat position');
+    print(' : </span>');
+    if (isset($content['field_window_seat']['#items'][0]['value'])) {
+        if ("1" == $content['field_window_seat']['#items'][0]['value']) {
+                print("<img src='/sites/all/themes/bootsass/assets/images/icon-window-on.png' width='20' height='20' style='vertical-align: top;' /> ");
+                print t('Window');
+        } else {
+                print("<img src='/sites/all/themes/bootsass/assets/images/icon-aisle-on.png' width='20' height='20' style='vertical-align: top;' /> ");
+                print t('Aisle/Middle');
+        }
+     }
+    print ('</li>');
+
+  }
+  
+?>
+
+
